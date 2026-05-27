09ca735fa66be3bff52b3334f982ee7be43a633b
kapilkumarcareer1-ux <kapilkumar.career1@gmail.com>
2026-05-27 08:19:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+  anchor.addEventListener('click', function (e) {
+
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href'))
+      .scrollIntoView({
+        behavior: 'smooth'
+      });
+
+  });
+
+});
+
+
+// Contact Form Alert
+document.getElementById("contactForm")
+.addEventListener("submit", function(e){
+
+  e.preventDefault();
+
+  alert("Message Sent Successfully!");
+
+});
